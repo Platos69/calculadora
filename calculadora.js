d85cdf0d443af5f5c604f6ef6f71cba037977c2e
Daniel <danielfelipe2905@gmail.com>
2023-08-05 11:55:29 -0300

    Bugs corrigidos e logaritimo adicionado

--- calculadora.js
+++ calculadora.js
@@ -1,31 +1,42 @@
 const loopOperacoesBasicas = ['1', '2', '3', '4']
-const loopOperacoesAvancadas = ['5']
+const loopOperacoesAvancadas = ['5', '6', '7', '8']
 
-function Receber_Valor(){
-let escolhaNumero 
+function Receber_Valor() {
+let escolhaNumero
 
-    do{
-        escolhaNumero = prompt('Tabela de operações:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Porcentagem\nDigite a opção desejada: ')
+escolhaNumero = prompt('Tabela de operações:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Porcentagem\n6 - Potenciação\n7 - Raíz Quadrada\n8 - Logaritimo\nDigite a opção desejada: ')
     
-        if(!loopOperacoesAvancadas.includes(escolhaNumero) || !loopOperacoesBasicas.includes(escolhaNumero)){
-            alert('O item selecionado é inválido!')
-            escolhaNumero = prompt('Tabela de operações:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Porcentagem\nDigite a opção desejada: ')
-        }
-    } while(!loopOperacoesAvancadas.includes(escolhaNumero) || !loopOperacoesBasicas.includes(escolhaNumero))
-
-    Efetuar_Operacoes(escolhaNumero)
-
+if(!loopOperacoesAvancadas.includes(escolhaNumero) && !loopOperacoesBasicas.includes(escolhaNumero)) {
+    do {
+    alert('Seleção inválida. Digite um valor numérico')
+    escolhaNumero = prompt('Tabela de operações:\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Porcentagem\n6 - Potenciação\n7 - Raíz Quadrada\n8 - Logaritimo\nDigite a opção desejada: ')
+    } while (!loopOperacoesAvancadas.includes(escolhaNumero) && !loopOperacoesBasicas.includes(escolhaNumero))
 }
 
-function Efetuar_Operacoes(escolhaNumero){
+Efetuar_Operacoes(escolhaNumero)
+}
+
+function Efetuar_Operacoes(escolhaNumero) {
     //variaveis base
     let numeroPrimeiro, numeroSegundo, resultado
    
-    if(loopOperacoesBasicas.includes(escolhaNumero)){
-        numeroPrimeiro = parseFloat(prompt('Digite o valor do primeiro número: '))
-        numeroSegundo = parseFloat(prompt('Digite o valor do segundo número: '))
+    if(loopOperacoesBasicas.includes(escolhaNumero)) {
+        numeroPrimeiro = Number(prompt('Digite o valor do primeiro número: '))
+
+        while (isNaN(numeroPrimeiro)) {
+            alert('Valor inválido. Digite um valor numérico.')
+            numeroPrimeiro = Number(prompt('Digite o valor do primeiro número: '))
+        }
+
+        numeroSegundo = Number(prompt('Digite o valor do segundo número: '))
+
+        while (isNaN(numeroPrimeiro)) {
+            alert('Valor inválido. Digite um valor numérico.')
+            numeroSegundo = Number(prompt('Digite o valor do segundo número: '))
+        }
+
         
-        switch(escolhaNumero){ 
+        switch(escolhaNumero) { 
             case '1':
                 resultado = numeroPrimeiro + numeroSegundo
                 alert(`A soma é igual à ${resultado.toFixed(2)}`)
@@ -46,10 +57,16 @@ function Efetuar_Operacoes(escolhaNumero){
                 alert(`A divisão é igual à ${resultado.toFixed(2)}`)
                 break
         }
-    }else if(loopOperacoesAvancadas.includes(escolhaNumero)){
+    }else if(loopOperacoesAvancadas.includes(escolhaNumero)) {
         switch (escolhaNumero) {
             case '5':
-                numeroPrimeiro = parseFloat(prompt('Digite o valor do número: '))
+                numeroPrimeiro = Number(prompt('Digite o valor do número: '))
+
+                while (isNaN(numeroPrimeiro)) {
+                    alert('Valor inválido. Digite um valor numérico.')
+                    numeroPrimeiro = Number(prompt('Digite o valor do número: '))
+                }
+
                 let porcentagemTipo = prompt('Tabela de tipos:\n1 - Acréscimo\n2 - Decréscimo\nDigite a opção desejada: ')
                 
                 while (porcentagemTipo !== '1' && porcentagemTipo !== '2') {
@@ -57,21 +74,67 @@ function Efetuar_Operacoes(escolhaNumero){
                     porcentagemTipo = prompt('Tabela de tipos:\n1 - Acréscimo\n2 - Decréscimo\nDigite a opção desejada: ')
                 }
 
-                let porcentagem = parseFloat(prompt('Digite a porcentagem: '))
+                let porcentagem = Number(prompt('Digite a porcentagem: '))
 
                 while (isNaN(porcentagem)) {
-                    alert('Porcentagem inválida. Digite um valor numérico.')
-                    porcentagem = parseFloat(prompt('Digite a porcentagem: '))
+                    alert('Valor inválido. Digite um valor numérico.')
+                    porcentagem = Number(prompt('Digite a porcentagem: '))
                 }
 
                 if (porcentagemTipo === '1') {
                     resultado = (1 + porcentagem / 100) * numeroPrimeiro
                     alert(`O resultado com acréscimo é: ${resultado.toFixed(2)}`)
                 } else if (porcentagemTipo === '2') {
-                    resultado = (porcentagem / 100) * numeroPrimeiro
+                    resultado = (1 - porcentagem / 100) * numeroPrimeiro
                     alert(`O resultado com decréscimo é: ${resultado.toFixed(2)}`)
                 }
                 break
+            
+            case '6':
+                numeroPrimeiro = Number(prompt('Digite o valor número: '))
+
+                while (isNaN(numeroPrimeiro)) {
+                    alert('Valor inválido. Digite um valor numérico.')
+                    numeroPrimeiro = Number(prompt('Digite o valor número: '))
+                }
+
+                let potenciaValor = Number(prompt('Digite a potência: '))
+                
+                while (isNaN(potenciaValor)) {
+                    alert('Valor inválido. Digite um valor numérico.')
+                    potenciaValor = Number(prompt('Digite a potência: '))
+                }
+
+                resultado = numeroPrimeiro ** potenciaValor
+
+                alert(`${numeroPrimeiro} elevado à ${potenciaValor} é igual à: ${resultado.toFixed(2)}`)
+                break
+
+            case '7':
+                    numeroPrimeiro = Number(prompt('Digite o valor do número: '))
+
+                    while (isNaN(numeroPrimeiro)) {
+                        alert('Valor inválido. Digite um valor numérico.')
+                        numeroPrimeiro = Number(prompt('Digite o valor do número: '))
+                    }
+    
+                    resultado = Math.sqrt(numeroPrimeiro)
+    
+                    alert(`A raiz quadrada de ${numeroPrimeiro} é: ${resultado.toFixed(3)}`)
+                    break
+
+            case '8':
+                    numeroPrimeiro = Number(prompt('Digite o valor do número: '))
+
+                    while (isNaN(numeroPrimeiro)) {
+                        alert('Valor inválido. Digite um valor numérico.')
+                        numeroPrimeiro = Number(prompt('Digite o valor do número: '))
+                    }
+    
+                    resultado = Math.log(numeroPrimeiro)
+    
+                    alert(`O log de ${numeroPrimeiro} é: ${resultado.toFixed(3)}`)
+                    break
         }
     }
 }
